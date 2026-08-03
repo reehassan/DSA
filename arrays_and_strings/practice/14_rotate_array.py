4eeca59bd21929c4000813b5af635373b7151f8b
reehassan <theailaboratories@gmail.com>
2026-08-03 12:26:18 +0500

    189. Rotate Array

---
+++ arrays_and_strings/practice/14_rotate_array.py
@@ -0,0 +1,11 @@
+# 1. Let n = length of nums
+
+# 2. Set k = k % n
+
+# 3. Reverse the entire array
+
+# 4. Reverse the first k elements
+#    (indices 0 to k-1)
+
+# 5. Reverse the remaining elements
+#    (indices k to n-1)

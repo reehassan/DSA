dbe010a22cae194871bf5254a5b21f69c450c92a
reehassan <theailaboratories@gmail.com>
2026-08-08 16:16:00 +0500

    153. Find Minimum in Rotated Sorted Array

---
+++ week1/03_minimum_in_rotated_sorted_arrya.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMin(self, nums):
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        return nums[left]
+
+# Time Complexity = O(log n)
+# Space Complexity = O(n)
